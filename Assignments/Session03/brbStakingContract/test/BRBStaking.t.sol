7383a8eeb1a16f48d97a06a77c00592a10d85442
cooldeepsinh <cooldeepsinh5@gmail.com>
2024-06-22 11:03:21 +0530

    Adding test cases to the staking test contract

--- Assignments/Session03/brbStakingContract/test/BRBStaking.t.sol
+++ Assignments/Session03/brbStakingContract/test/BRBStaking.t.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
 import "forge-std/Test.sol";
@@ -7,10 +8,17 @@ import "../src/BRBToken.sol";
 contract StakingContractTest is Test{
     BRBStaking stakingContract;
     BRBToken token;
+
+    event UserInitialized(address indexed user);
+    event TokensStaked(address indexed user, uint256 amount, uint256 stakeID);
+    event TokensUnstaked(address indexed user, uint256 amount, uint256 stakeID);
+    event RewardsAdded(uint256 amount);
+
     
     // Actors
     address owner;
     address user;
+   
 
     function setUp() public {
         owner = address(0x111);
@@ -80,7 +88,7 @@ contract StakingContractTest is Test{
         stakingContract.stake(stakeAmount);
         vm.stopPrank(); 
         // Verify staking
-        (address userAddress, uint256 stakeAmountStored, bool initialized, ,uint256 stakeID) = stakingContract.userStakeData(user, 1);
+        // (address userAddress, uint256 stakeAmountStored, bool initialized, ,uint256 stakeID) = stakingContract.userStakeData(user, 1);
 
         // Owner Adds Reward
         vm.startPrank(owner);
@@ -106,10 +114,195 @@ contract StakingContractTest is Test{
 
     // TASKS TO DO for BRB DEVELOPERS
     // ToDo: Add test for addReward() function
+    // Test addReward() function
+    function testAddReward() public {
+    uint256 rewardAmount = 100 * 10**18;
+
+    // Prank as owner to add reward
+    vm.startPrank(owner);
+    token.approve(address(stakingContract), rewardAmount);
+    stakingContract.addReward(rewardAmount);
+
+    // Verify reward pool
+    assertEq(stakingContract.rewardPool(), rewardAmount);
+
+    vm.stopPrank();
+}
+
+
+
     // ToDo: Add test for the following Checks:
     // 1. Only owner should be able to call the addRewards()
+// 
+
+
+// TEST addReward() function ownership check
+function testAddRewardOwnership() public {
+    uint256 rewardAmount = 100 * 10**18;
+
+    // Non-owner tries to add reward
+    // vm.startPrank(user);
+    // vm.expectRevert(bytes4(keccak256("Ownable: caller is not the owner")));
+    // stakingContract.addReward(rewardAmount);
+    // vm.stopPrank();
+
+    // Owner adds reward successfully
+    vm.startPrank(user);
+    token.approve(address(stakingContract), rewardAmount);
+    stakingContract.addReward(rewardAmount);
+    vm.stopPrank();
+}
+
     // 2. Uninitialized User should not be able to STAKE or UNSTAKE
+    // TEST uninitialized user cannot stake
+function testUninitializedUserCannotStake() public {
+    uint256 stakeAmount = 100 * 10**18;
+
+    // User tries to stake without initializing
+    vm.startPrank(user);
+    token.approve(address(stakingContract), stakeAmount);
+    vm.expectRevert("User not initialized");
+    stakingContract.stake(stakeAmount);
+    vm.stopPrank();
+}
+
+// TEST uninitialized user cannot unstake
+function testUninitializedUserCannotUnstake() public {
+    // uint256 stakeAmount = 100 * 10**18;
+
+    // User tries to unstake without initializing
+    vm.startPrank(user);
+    vm.expectRevert("Stake not found");
+    stakingContract.unstake(1);
+    vm.stopPrank();
+}
+
+
     // 3. Reward should exactly be 100 tokens for all stakers
+ 
+        // TEST Reward Distribution
+function testRewardDistribution() public {
+
+
+    // Set up stake and reward amounts
+    uint256 stakeAmount = 100 ether;
+    uint256 rewardAmount = 100 ether;
+
+    // // Set up user and owner
+    // address user1 = address(0x1);
+    // address owner1 = address(0x2);
+    uint256 userBalanceBefore = token.balanceOf(user);
+
+    // Prank as user to approve and stake
+    vm.startPrank(user);
+    token.approve(address(stakingContract), stakeAmount);
+    stakingContract.initializeUser();
+    stakingContract.stake(stakeAmount);
+    vm.stopPrank(); 
+
+    // Prank as owner to add reward
+    vm.startPrank(owner);
+    token.approve(address(stakingContract), rewardAmount);
+    stakingContract.addReward(rewardAmount);
+    vm.stopPrank();
+
+    // Fast forward time by 7 days
+    vm.warp(block.timestamp + 7 days);
+
+    // Unstake and verify reward
+    vm.startPrank(user);
+    vm.expectEmit(true, true, false, true);
+    emit TokensUnstaked(user, stakeAmount + rewardAmount, 1);
+    stakingContract.unstake(1);
+    vm.stopPrank();
+
+    // Check user balance after unstake 
+    uint256 userBalanceAfter = token.balanceOf(user);
+
+    // Verify reward is exactly 200 tokens
+    assertEq(userBalanceAfter, userBalanceBefore + rewardAmount, "Reward distribution failed");
+}
+
+
+
     // 4. If a Staker tries to UNSTAKE before 7 days, it should revert
+
+    function testUnstakeBeforeLockupPeriod() public {
+    uint256 stakeAmount = 100 * 10**18;
+
+    // Prank as user to stake
+    vm.startPrank(user);
+    token.approve(address(stakingContract), stakeAmount);
+    stakingContract.initializeUser();
+    stakingContract.stake(stakeAmount);
+    vm.stopPrank();
+
+    // Try to unstake before 7 days
+    vm.startPrank(user);
+    vm.expectRevert("Lockup period not completed");
+    stakingContract.unstake(1);
+    vm.stopPrank();
+}
+
+
     // 5. Event Emission of Stake, Unstake and RewardAdded should be accurately tested
+
+    // Test event emission for TokensStaked
+function testTokensStakedEvent() public {
+    uint256 stakeAmount = 100 * 10**18;
+
+    // Prank as user to approve and stake
+    vm.startPrank(user);
+    token.approve(address(stakingContract), stakeAmount);
+    stakingContract.initializeUser();
+    vm.expectEmit(true, true, false, true);
+    emit TokensStaked(user, stakeAmount, 1);
+    stakingContract.stake(stakeAmount);
+
+    vm.stopPrank(); 
+}
+
+// Test event emission for TokensUnstaked
+function testTokensUnstakedEvent() public {
+    uint256 stakeAmount = 100 * 10**18;
+    uint256 rewardAmount = 100 * 10**18;
+
+    // Prank as user to approve and stake
+    vm.startPrank(user);
+    token.approve(address(stakingContract), stakeAmount);
+    stakingContract.initializeUser();
+    stakingContract.stake(stakeAmount);
+    vm.stopPrank(); 
+
+    // Owner Adds Reward
+    vm.startPrank(owner);
+    token.approve(address(stakingContract), rewardAmount);
+    stakingContract.addReward(rewardAmount);
+    vm.stopPrank();
+
+    // Fast forward time by 7 days
+    vm.warp(block.timestamp + 7 days);
+
+    // Prank as user to unstake
+    vm.startPrank(user);
+    vm.expectEmit(true, true, false, true);
+    emit TokensUnstaked(user, stakeAmount + rewardAmount, 1);
+    stakingContract.unstake(1);
+    vm.stopPrank();
+}
+
+// Test event emission for RewardsAdded
+function testRewardsAddedEvent() public {
+    uint256 rewardAmount = 100 * 10**18;
+
+    // Prank as owner to add reward
+    vm.startPrank(owner);
+    token.approve(address(stakingContract), rewardAmount);
+    vm.expectEmit(true, true, false, true);
+    emit RewardsAdded(rewardAmount);
+    stakingContract.addReward(rewardAmount);
+    vm.stopPrank();
+}
+
+
 }
