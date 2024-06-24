bfc814bf90ae248f2f1dd6e98a163ad108bfc5ab
Shozaib-Khan <70322458+Shozaib-Khan@users.noreply.github.com>
2024-06-25 01:40:41 +0530

    Create pheonixz.sol

---
+++ Bootcampers/Shozaib_Khan/Session2/pheonixz.sol
@@ -0,0 +1,16 @@
+// contracts/pheonixz.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract pheonixz is ERC20, Ownable {
+    constructor(uint256 initialSupply) ERC20("PheonixzTKO", "DRK") Ownable(msg.sender) {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+}
