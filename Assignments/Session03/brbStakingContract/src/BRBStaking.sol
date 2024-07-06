e5eab516e4b63bda830dd39d90fec6e1cf6df9f4
daiwikmaheshwari <daiwikdomain@gmail.com>
2024-07-06 14:56:24 +0530

    ERC-20

--- Assignments/Session03/brbStakingContract/src/BRBStaking.sol
+++ Assignments/Session03/brbStakingContract/src/BRBStaking.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts@5.0.2/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts@5.0.2/access/Ownable.sol";
 
-contract Blackhole is ERC20, Ownable {
+contract blackhole is ERC20, Ownable {
     constructor(
         address initialOwner
     ) ERC20("blackhole", "BH") Ownable(initialOwner) {}
