e3dd5c8d3e2889d2358a2282aaa0a3faaddcb171
daiwikmaheshwari <daiwikdomain@gmail.com>
2024-07-06 17:31:21 +0530

    daiwikmaheshwari

---
+++ Bootcampers/Daiwik_Maheshwari/ERC20TOKEN.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts@5.0.2/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@5.0.2/access/Ownable.sol";
+
+contract blackhole is ERC20, Ownable {
+    constructor(
+        address initialOwner
+    ) ERC20("blackhole", "BH") Ownable(initialOwner) {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
